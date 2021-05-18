5f8fb32c2ab2fd277f265641159e0b5475cf83b6
Taylor_Atkins <43218109+TaylorAtkins@users.noreply.github.com>
2021-05-18 12:26:03 -0700

    Added play button to main component

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,11 +1,19 @@
 #include "MainComponent.h"
 
 //==============================================================================
-MainComponent::MainComponent():juce::Component()
+MainComponent::MainComponent() : juce::Component()
 {
-    setSize (600, 400);
-    addAndMakeVisible(metronome);
-    metronome.newRhythm();
+    setSize(600, 400);
+    addAndMakeVisible(playButton);
+    playButton.setButtonText ("Play!");
+    playButton.addListener (this);
+    playButton.setColour(juce::TextButton::buttonColourId, juce::Colour(0xFFFFFFFF));
+   // playButton.setColour(juce::TextButton::buttonOnColourId, juce::Colour(0xFFFF658A));
+    playButton.setColour(juce::TextButton::textColourOffId, juce::Colour(0xFF000000));
+    //playButton.setColour(juce::TextButton::textColourOffId, juce::Colour(0xFFFF658A));
+    addChildComponent(metronome);
+    //addAndMakeVisible(metronome);
+    //metronome.newRhythm();
 }
 
 MainComponent::~MainComponent()
@@ -13,14 +21,9 @@ MainComponent::~MainComponent()
 }
 //==============================================================================
 
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::paint(juce::Graphics &g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (juce::Colours::blue);
-
-    //g.setFont (juce::Font (16.0f));
-    //g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);A
-
+    g.fillAll(juce::Colour(0xFF000000));
 }
 
 void MainComponent::resized()
@@ -28,5 +31,21 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    metronome.setBounds (0, 0, getWidth(), getHeight());
+    metronome.setBounds(0, 0, getWidth(), getHeight());
+    int buttonHeight = getHeight()/8;
+    //playButton.changeWidthToFitText(buttonHeight);
+    int buttonWidth = getWidth()/4;
+    playButton.setBounds(getWidth() / 2 - buttonWidth/2, getHeight()/ 2 - buttonHeight/2, buttonWidth, buttonHeight);
+}
+
+void MainComponent::buttonClicked(juce::Button *button)
+{
+    // If play button is clicked disable button and enable settings buttons
+    if(button == &playButton)
+    {
+        playButton.setEnabled(false);
+        playButton.setVisible(false);
+        metronome.setVisible(true);
+        metronome.newRhythm();
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -3,23 +3,24 @@
 #include <JuceHeader.h>
 #include "MetronomeComponent.h"
 
-
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
 //==============================================================================
-class MainComponent  : public juce::Component
+class MainComponent : public juce::Component, public juce::Button::Listener
 {
 public:
     MainComponent();
     ~MainComponent() override;
-    void paint (juce::Graphics&) override;
+    void buttonClicked(juce::Button *button) override;
+    void paint(juce::Graphics &) override;
     void resized() override;
 
 private:
     MetronomeComponent metronome;
+    juce::TextButton playButton;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
